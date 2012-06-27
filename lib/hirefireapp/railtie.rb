3bfa85979cc542c945e516b0b8042d268f7c0464
JP Lewicke <jplewicke@gmail.com>
2012-06-27 06:44:45 -0400

    Try a different position in the Midddleware stack.

--- lib/hirefireapp/railtie.rb
+++ lib/hirefireapp/railtie.rb
@@ -3,7 +3,7 @@
 module HireFireApp
   class Railtie < ::Rails::Railtie
     initializer 'hirefireapp.add_middleware' do |app|
-      app.config.middleware.insert_before('Rack::Cache', 'HireFireApp::Middleware')
+      app.config.middleware.insert_before('Rack::ConditionalGet', 'HireFireApp::Middleware')
     end
   end
 end
